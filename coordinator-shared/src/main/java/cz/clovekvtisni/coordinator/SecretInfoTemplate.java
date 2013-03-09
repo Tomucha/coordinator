13279362da30a34b67f51309e835d8b0bd8e1c94
Franta Hejl <fhucho@gmail.com>
2013-03-09 18:47:04 +0100

    Added SecretInfoTemplate.java.

---
+++ coordinator-shared/src/main/java/cz/clovekvtisni/coordinator/SecretInfoTemplate.java
@@ -0,0 +1,9 @@
+package cz.clovekvtisni.coordinator;
+
+// Template for the file SecretInfo.java, which is not part of the version
+// control system, because it contains secrets like passwords and API keys.
+// Developers should create a copy in the same directory named SecretInfo.java
+// and fill the secret info.
+public class SecretInfoTemplate {
+    public static final String GCM_SENDER_ID = null;
+}
